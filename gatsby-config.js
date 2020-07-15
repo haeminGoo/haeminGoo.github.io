52f85dca73b7148c9d25d47da6e62faaeb52f7c1
haeminG00 <59075370+haeminG00@users.noreply.github.com>
2020-07-15 16:24:25 +0900

    make first resume

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Haemin Goo | Front-end Developer`,
+    author: `Haemin Goo`,
+    description: `With 5+ years of experience as front-end develper in various industries. I design and build various projects and I value making satisfactory result on time.`,
+    siteUrl: `https://haeminG00.github.io/`,
+    social: {
+      // twitter: `barancezayirli`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Baran Cezayirli | Technologist`,
+        short_name: `CV`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#38B2AC`,
+        display: `standalone`,
+        icon: 'src/assets/site-icon.png'
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true, // Enable tailwindcss support
+      }
+    }
+  ],
+};
