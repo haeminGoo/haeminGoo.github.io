ef3cba56ff056735d1b3ba1e601f3cec9cecc70b
Haemin (Lucy) <44049587+haemin0420@users.noreply.github.com>
2021-02-18 14:44:31 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8dc7108d10ab4ce7dbfe.js"
+    "url": "webpack-runtime-e31f50caf1125545243e.js"
   },
   {
     "url": "commons-8c010473a72c64d0f741.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d94970bcfb16308b7e0a06acdad9e431"
+    "revision": "006f2f219bde14f09ef6dc2e03c758be"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
